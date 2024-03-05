56e2167f43dda14218bdb806fd6fcc1fbddb0d6e
leonren19 <leonren19@gmail.com>
2024-03-04 19:21:02 -0800

    update

--- src/social.js
+++ src/social.js
@@ -1,1 +1,9 @@
-<h1> Messaging Center </h1>
+
+var messages=document.getElementById("messages")
+var textbox=document.getElementById("textbox")
+var send=document.getElementById("send")
+
+send.addEventListener("click", function(){ 
+    var newMessage=document.createElement
+    newMessage.innerHTML=textbox
+})
